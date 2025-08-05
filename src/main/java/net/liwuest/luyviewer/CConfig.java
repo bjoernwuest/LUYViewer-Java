3d619e6f9ee526891cbd868da83a0cbea17a81f0
Bjoern Wuest <bjoern@liwuest.net>
2025-08-05 09:31:18 +0200

    Step-by-step debugging

---
+++ src/main/java/net/liwuest/luyviewer/CConfig.java
@@ -0,0 +1,15 @@
+package net.liwuest.luyviewer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Configuration model for the LUYViewer application.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CConfig {
+  @JsonProperty("luy_host") public String luyHost;
+  @JsonProperty("language") public String language;
+
+  public CConfig() {}
+}
